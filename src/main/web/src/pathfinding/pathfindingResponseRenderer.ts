13756c0ced77d702c731dad70bf8579ed0bf68f2
Evan Kirsch <kirsch.j.evan@gmail.com>
2026-02-16 14:22:08 -0600

    Add clear response to pathfinding renderer

--- src/main/web/src/pathfinding/pathfindingResponseRenderer.ts
+++ src/main/web/src/pathfinding/pathfindingResponseRenderer.ts
@@ -2,16 +2,20 @@ import { MapRenderer } from "../map/mapRenderer"
 
 export class PathfindingResponseRenderer {
 
+  static polyline: google.maps.Polyline;
+  static markers: google.maps.marker.AdvancedMarkerElement[] = [];
+
   async renderResponse(response) {
+    this.clearResponse();
     const data: PathfindingResponse = await response.json();
     const { AdvancedMarkerElement } = await google.maps.importLibrary("marker") as google.maps.MarkerLibrary;
     const { Map } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
     data.legs.forEach((place) => {
-      new AdvancedMarkerElement({
+      PathfindingResponseRenderer.markers.push(new AdvancedMarkerElement({
         map: MapRenderer.map,
         position: { lat: place.origin.lat, lng: place.origin.lng },
         title: place.origin.displayName,
-      });
+      }));
     });
     const {encoding} = (await google.maps.importLibrary("geometry")) as google.maps.GeometryLibrary;
     const decodedPath = encoding.decodePath(data.encodedPolyline);
@@ -21,8 +25,17 @@ export class PathfindingResponseRenderer {
       strokeOpacity: 1,
       strokeWeight: 3
     }
-    var polyline = new google.maps.Polyline(polyOptions);
-    polyline.setMap(MapRenderer.map);
+    PathfindingResponseRenderer.polyline = new google.maps.Polyline(polyOptions);
+    PathfindingResponseRenderer.polyline.setMap(MapRenderer.map);
+  }
+
+  private clearResponse() {
+    if (PathfindingResponseRenderer.polyline != undefined) {
+      PathfindingResponseRenderer.polyline.setMap(null);
+    }
+
+    PathfindingResponseRenderer.markers.forEach(elt => elt.map = null);
+    PathfindingResponseRenderer.markers = [];
   }
 
 }
