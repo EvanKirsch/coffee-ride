f47a8bf036f16e442ea2a40766896500b226d6b2
Evan Kirsch <kirsch.j.evan@gmail.com>
2026-02-27 13:16:35 -0600

    Add logger to exception handler

--- src/main/java/org/kirsch/controller/CoffeeRideExceptionHandler.java
+++ src/main/java/org/kirsch/controller/CoffeeRideExceptionHandler.java
@@ -1,5 +1,7 @@
 package org.kirsch.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -8,8 +10,11 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class CoffeeRideExceptionHandler {
 
+  private static final Logger log = LoggerFactory.getLogger(CoffeeRideExceptionHandler.class);
+
   @ExceptionHandler(Exception.class)
   public ResponseEntity<String> handleError(Exception e) {
+    log.warn("Handling Response Error : {}", e.getMessage());
     return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
   }
 
