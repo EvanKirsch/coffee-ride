d4c49dced90add79e6de5e1abb7d6e288d19049d
Evan Kirsch <kirsch.j.evan@gmail.com>
2026-03-02 09:04:34 -0600

    Add logger to debug util

--- src/main/java/io/coffeeride/util/DebugUtil.java
+++ src/main/java/io/coffeeride/util/DebugUtil.java
@@ -1,13 +1,17 @@
 package io.coffeeride.util;
 
-import com.google.maps.places.v1.Place;
+import io.coffeeride.model.CoffeeRidePlace;
 import java.util.List;
 import io.coffeeride.model.Node;
 import io.coffeeride.model.WeightedPlaceGraph;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public final class DebugUtil {
 
   private static DebugUtil debugUtil;
+
+  private static final Logger log = LoggerFactory.getLogger(DebugUtil.class);
 
   private DebugUtil() {
 
@@ -20,17 +24,23 @@ public final class DebugUtil {
     return debugUtil;
   }
 
-  public void printPlaces(List<Place> places) {
-    for (Place place : places) {
-      System.out.println( place.getDisplayName().getText() + ", " + place.getFormattedAddress());
+  public void printPlaces(List<CoffeeRidePlace> places) {
+    for (CoffeeRidePlace place : places) {
+      log.info("{}, {}",
+          place.getDisplayName(),
+          place.getAddress()
+      );
     }
   }
 
   public void printGraph(WeightedPlaceGraph graph) {
     for (Node node : graph.getNodes()) {
-      System.out.println(node.getDistanceToTerminus() + ", " + node.getDistanceToStart()
-          + ", " + node.getPlace().getDisplayName().getText()
-          + ", " + node.getPlace().getFormattedAddress());
+      log.info("{}, {}, {}, {}",
+          node.getDistanceToTerminus(),
+          node.getDistanceToStart(),
+          node.getPlace().getDisplayName().getText(),
+          node.getPlace().getFormattedAddress()
+      );
     }
   }
 
